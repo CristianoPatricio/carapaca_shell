ea44374a52e39ccfed7cec6bc8216792f68fc7db
CristianoPatricio <cristianop1998@gmail.com>
2020-05-26 14:59:08 +0100

    Gen SSH Keys completed

--- Client/Client.java
+++ Client/Client.java
@@ -1,22 +1,13 @@
 import java.net.*;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.X509EncodedKeySpec;
 import java.io.*;
 import java.util.Scanner;
-
-class DSAKeyPair {
-
-    PrivateKey priv;
-    PublicKey pub;
-
-    DSAKeyPair(PrivateKey sk, PublicKey pk) {
-        priv = sk;
-        pub = pk;
-    }
-}
 
 public class Client 
 {
@@ -34,22 +25,14 @@ public class Client
     private void start() throws IOException
     {
         String input = "";
-
-        PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        // Get Private and Public Key
-        DSAKeyPair pubKey = getKeyPair();
-        PublicKey pk = pubKey.pub;
-        
-        // Send pk to Server
-        sendPubKeyToServer(pk, out);
         
         while (!input.equals("exit")) {
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
             input = scanner.nextLine();
             out.println(input);
             out.flush();
 
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String messageFromServer = in.readLine();
             whileChatting(messageFromServer);
         }
@@ -66,40 +49,78 @@ public class Client
         System.out.println("\r\nMessage from Server to Client: " + messageFromServer);
     }
 
-    private DSAKeyPair getKeyPair() {
+    /**
+     * Sends the pub key to the Server
+     * @param client
+     * @param pk
+     * @throws IOException
+     */
+    private static void sendPubKeyToServer(Client client, PublicKey pk) throws IOException {
+        
+        try {
+            PrintWriter out = new PrintWriter(client.socket.getOutputStream(), true);
 
-        /* Generate Public and Private Keys */
+            // Send pk to Server
+            byte[] key = pk.getEncoded();
+            out.println(key);
+        } catch (Exception e) {
+            System.out.println("Caught exception: " + e.toString());
+        }
+        
+    }
+
+    /**
+     * 
+     * @return public key
+     */
+    private static PublicKey getPubKey() {
 
         try {
-            // Create a Key Pair Generator
-            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+            // get pub key and send it to the Server
+            FileInputStream keyfis = new FileInputStream("pub_key");
+            byte[] encKey = new byte[keyfis.available()];
+            keyfis.read(encKey);
+            keyfis.close();
 
-            // Initialize the Key Pair Generator
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-            keyGen.initialize(1024, random);
+            // Obtain a key specification
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
 
-            // Generate the Pair of Keys
-            KeyPair pair = keyGen.generateKeyPair();
-            PrivateKey priv = pair.getPrivate();
-            PublicKey pub = pair.getPublic();
+            // KeyFactory to do the conversion
+            KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
 
-            return new DSAKeyPair(priv, pub);
+            // Generate a PublicKey from the key specification
+            PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
 
+            return pubKey;
         } catch (Exception e) {
             System.out.println("Caught exception: " + e.toString());
             return null;
         }
     }
 
-    private void sendPubKeyToServer(PublicKey pub, PrintWriter out) {
-        // Send pk to Server
-        byte[] key = pub.getEncoded();
-        out.println(key);
-    }
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws Exception 
-    {
         Client client = new Client(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+
+        // Verify if the file that contains the public key exists
+        File f = new File("pub_key");
+        if (f.isFile()) {
+            // Get Public Key From File
+            PublicKey pk = getPubKey();
+
+            // Send pub key to client
+            sendPubKeyToServer(client, pk);
+        } else {
+            // Generate key pair and creates the files
+            GenKeyPairs genKeys = new GenKeyPairs();
+            genKeys.genKeyPairs();
+
+            // Get Public Key From File
+            PublicKey pk = getPubKey();
+
+            // Send pub key to client
+            sendPubKeyToServer(client, pk);
+        }
 
         System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
         client.start();
